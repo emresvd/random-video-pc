8b6993d3d09a1b41721e77541c13cd13fe1d2e0f
emresvd <emresevindi99@gmail.com>
2022-10-16 17:48:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
+
 import sys
 import random_video
 
-YOUTUBE_EMBED_URL="https://www.youtube.com/embed/"
-
+YOUTUBE_EMBED_URL = "https://www.youtube.com/embed/{}?autoplay=1&mute=1"
 
 class Window(QMainWindow):
     def __init__(self):
@@ -15,12 +15,14 @@ class Window(QMainWindow):
         self.setWindowTitle("Random YouTube Video")
         self.setWindowIcon(QIcon("icon.png"))
         self.setCentralWidget(self.web)
-        self.web.load(QUrl(YOUTUBE_EMBED_URL + random_video.get_random_video_id()))
+        self.web.load(QUrl(self.getUrl()))
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Control:
-            self.web.load(QUrl(YOUTUBE_EMBED_URL + random_video.get_random_video_id()))
+            self.web.load(QUrl(self.getUrl()))
 
+    def getUrl(self):
+        return YOUTUBE_EMBED_URL.format(random_video.get_random_video_id())
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)

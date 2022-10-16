b7b6add86e594b8ce3564e508f9926935ccad20c
emresvd <emresevindi99@gmail.com>
2022-10-16 17:39:46 +0300

    update 22

--- main.py
+++ main.py
@@ -5,25 +5,25 @@ from PyQt5.QtWebEngineWidgets import *
 import sys
 import random_video
 
+YOUTUBE_EMBED_URL="https://www.youtube.com/embed/"
+
 
 class Window(QMainWindow):
-    def __init__(self, url):
+    def __init__(self):
         super(Window, self).__init__()
         self.web = QWebEngineView()
-        self.web.load(QUrl(url))
-        self.setWindowTitle("Random Video")
+        self.setWindowTitle("Random YouTube Video")
         self.setWindowIcon(QIcon("icon.png"))
         self.setCentralWidget(self.web)
+        self.web.load(QUrl(YOUTUBE_EMBED_URL + random_video.get_random_video_id()))
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Control:
-            self.web.load(
-                QUrl("https://www.youtube.com/embed/" + random_video.get_random_video_id()))
+            self.web.load(QUrl(YOUTUBE_EMBED_URL + random_video.get_random_video_id()))
 
 
 if __name__ == '__main__':
-    url = "https://www.youtube.com/embed/" + random_video.get_random_video_id()
     app = QApplication(sys.argv)
-    window = Window(url)
+    window = Window()
     window.show()
     sys.exit(app.exec())
--- random_video.py
+++ random_video.py
@@ -1,11 +1,13 @@
-from youtube_search import YoutubeSearch
-from bs4 import BeautifulSoup
+import youtube_search
+import youtubesearchpython
+import bs4
 import requests
 import random
+import datetime
 
 
 def get_random_number():
-    number = random.randrange(0, 10000)
+    number = random.randrange(10000)
     if number >= 1000:
         return str(number)
     if number >= 100:
@@ -16,63 +18,115 @@ def get_random_number():
         return "000" + str(number)
 
 
-def get_random_img_search():
-    if random.choice([True, False]):
-        return "IMG " + get_random_number()
-    else:
-        return "IMG_" + get_random_number()
+def get_random_p_search(p=random.choice(["img", "mvi", "mov", "dsc"])):
+    case = random.randrange(3)
+    p = p.upper()
+    if not case:
+        return "{} {}".format(p, get_random_number())
+    if case == 1:
+        return "{}_{}".format(p, get_random_number())
+    if case == 2:
+        return "{}{}".format(p, get_random_number())
 
 
-def get_random_web_search():
+def get_random_wiki_search():
     if random.choice([True, False]):
+        r = requests.get(
+            "https://en.wikipedia.org/wiki/Special:Random#/random")
+        soup = bs4.BeautifulSoup(r.content, "html.parser")
         if random.choice([True, False]):
-            r = requests.get("https://en.wikipedia.org/wiki/Main_Page")
-            soup = BeautifulSoup(r.content, "html.parser")
             words = []
-            for i in str(soup.text).split():
-                if i in words:
-                    continue
-                words.append(i)
+            for i in soup.find_all("p"):
+                for j in i.text.split():
+                    if j in words:
+                        continue
+                    words.append(j)
             return random.choice(words)
         else:
-            r = requests.get("https://en.m.wikipedia.org/wiki/Special:Random#/random")
-            soup = BeautifulSoup(r.content, "html.parser")
-            words = []
-            for i in str(soup.text).split():
-                if i in words:
-                    continue
-                words.append(i)
-            return random.choice(words)
+            return soup.title.text.split("-")[0]
     else:
-        r = requests.get("https://www.bbc.com/")
-        soup = BeautifulSoup(r.content, "html.parser")
-        words = []
-        for i in soup.find_all("a"):
-            if not i.get("class"):
-                continue
-            if "media__link" in i.get("class"):
-                if not i.text.strip() in words:
-                    words.append(i.text.strip())
-        return random.choice(words)
+        r = requests.get("https://en.wikipedia.org/wiki/Main_Page")
+        soup = bs4.BeautifulSoup(r.content, "html.parser")
+        return random.choice(soup.text.split())
+
+
+def get_random_vid_search():
+    year = random.randint(2009, datetime.datetime.now().year)
+    month = random.randint(1, 12)
+    day = random.randint(1, 30)
+    if year == datetime.datetime.now().year:
+        month = random.randint(1, datetime.datetime.now().month)
+    if month > datetime.datetime.now().month:
+        day = random.randint(1, datetime.datetime.now().day)
+    year = str(year)
+    if month < 10:
+        month = "0" + str(month)
+    month = str(month)
+    if day < 10:
+        day = "0" + str(day)
+    day = str(day)
+    return "vid {}{}{}".format(year, month, day)
+
+
+def get_new_video():
+    word = random.choice(["img", "vid", "mvi", "mov", "dsc"])
+    return random.choice(advanced_search(word, youtubesearchpython.VideoSortOrder.uploadDate))
+
+
+def normal_search(word):
+    results = youtube_search.YoutubeSearch(word, max_results=100).to_dict()
+    video_ids = []
+    for i in results:
+        video_ids.append(i["id"])
+    return video_ids
+
+
+def advanced_search(word, mode=None):
+    if not mode:
+        a = random.randrange(3)
+        if a == 0:
+            mode = youtubesearchpython.VideoSortOrder.uploadDate
+        if a == 1:
+            mode = youtubesearchpython.VideoSortOrder.viewCount
+        if a == 2:
+            mode = youtubesearchpython.VideoSortOrder.rating
+    results = youtubesearchpython.CustomSearch(word, mode, limit=100)
+    video_ids = []
+    for i in results.result().get("result"):
+        video_ids.append(i.get("id"))
+    return video_ids
+
+
+def get_word():
+    case = random.randrange(6)
+    if case == 0:
+        word = get_random_p_search("img")
+    if case == 1:
+        word = get_random_wiki_search()
+    if case == 2:
+        word = get_random_vid_search()
+    if case == 3:
+        word = get_random_p_search("mvi")
+    if case == 4:
+        word = get_random_p_search("mov")
+    if case == 5:
+        word = get_random_p_search("dsc")
+
+    return word
 
 
 def get_random_video_id():
+    if not random.randrange(7):
+        return get_new_video()
+
     while True:
-        if random.choice([True, False]):
-            word = get_random_img_search()
+        word = get_word()
+
+        if not random.randrange(4):
+            video_ids = normal_search(word)
         else:
-            try:
-                word = get_random_web_search()
-            except:
-                word = get_random_img_search()
+            video_ids = advanced_search(word)
 
-        results = YoutubeSearch(word, max_results=100).to_dict()
-
-        video_ids = []
-        for i in results:
-            video_ids.append(i["id"])
-
-        id = None
         try:
             id = random.choice(video_ids)
             break

1f7158bff3e23e15f471504599c41ad323063d06
emresvd <emresevindi99@gmail.com>
2022-10-18 18:23:38 +0300

    update requirements.txt

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 rd /s /q build
 rd /s /q dist
 del randomvideo.spec
-pyinstaller --windowed --name=randomvideo --icon=icon.ico --add-data="icon.png;." main.py
+venv\Scripts\pyinstaller.exe --noconsole --name=randomvideo --icon=icon.ico --add-data="icon.png;." --add-data="icon.ico;." main.py

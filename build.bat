a67ab0761976ebb65d602ea3dac34333783784a4
emre <109899306+emresvd@users.noreply.github.com>
2022-10-23 16:38:32 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@
 rd /s /q build
 rd /s /q dist
 del randomvideo.spec
-venv\Scripts\pyinstaller.exe --noconsole --name=randomvideo --icon=icon.ico --add-data="icon.png;." --add-data="icon.ico;." main.py
+pyinstaller --noconsole --name=randomvideo --icon=icon.ico --add-data="icon.png;." --add-data="icon.ico;." main.py
